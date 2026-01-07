8f823576e6a59f13c6b3aee4d3bc4313932cc7fb
Abosi-Godwin <bbnl6060@github.com>
2026-01-07 20:48:01 +0100

    updated

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return (
-        <section className="bg-visyon-light overflow-hidden h-dvh">
+        <section className="bg-visyon-light overflow-hidden">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="grid grid-cols-1 lg:grid-cols-2 items-center min-h-[600px] py-12 lg:py-0">
                     {/* Text Content */}
